c0d22e583b632d8aeb933c5ea85406e2fddad2bf
Maxence Fournier <mafourni@paul-f3Ar5s11.clusters.42paris.fr>
2025-01-23 19:22:43 +0100

    oe

--- exec/delocate_mem/freeingthis.c
+++ exec/delocate_mem/freeingthis.c
@@ -6,16 +6,16 @@
 /*   By: mafourni <mafourni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 18:28:37 by mafourni          #+#    #+#             */
-/*   Updated: 2025/01/22 19:06:14 by mafourni         ###   ########.fr       */
+/*   Updated: 2025/01/23 15:43:07 by mafourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/philo.h"
 
-void ft_free(t_philo *all)
-{
-    free(all->table);
-    free(all->ph_threads);
-    free(all->fork);
-    free(all);
-}
+// void ft_free(t_philo *all)
+// {
+//     free(all->table);
+//     free(all->ph_threads);
+//     free(all->fork);
+//     free(all);
+// }
--- exec/main.c
+++ exec/main.c
@@ -6,7 +6,7 @@
 /*   By: mafourni <mafourni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 20:50:44 by codespace         #+#    #+#             */
-/*   Updated: 2025/01/22 19:06:28 by mafourni         ###   ########.fr       */
+/*   Updated: 2025/01/23 19:16:28 by mafourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,27 +24,6 @@ int main(int argc, char **argv)
 	if (philo == NULL)
 		return (printf("Failed to create philo\n"), -1);
 	monitor(philo);
-	join_and_destroy_mutex(all);
-	ft_free(all);
-	return (0);
-}
-
-
-int		join_and_destroy_mutex(t_philo *all)
-{
-	int i = 0;
-	int philos = all->number_of_philosophers;
-	int j = 0;
-	while( j < philos)
-	{
-		if (pthread_join(all->philo_threads[j], NULL)!= 0)
-			return(printf("Failed to join\n"), -1);
-		j++;
-	}
-	while (i < philos)
-	{
-		pthread_mutex_destroy(&all->fork[i]);
-		i++;
-	}
+	// ft_free(all);
 	return (0);
 }
